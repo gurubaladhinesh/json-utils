d1cba1893d29242a0e1dcf40fed0a77e40771b16
gurubaladhinesh <gurubaladhinesh@gmail.com>
2020-07-28 09:10:45 +0530

    initial commit

---
+++ src/main/java/com/techguru/json/diff/JsonDiff.java
@@ -0,0 +1,43 @@
+package com.techguru.json.diff;
+
+import java.util.AbstractMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class JsonDiff {
+
+    private JsonDiff() {
+    }
+
+    public static Map<String, Object> flatten(Map<String, Object> map) {
+        return map.entrySet()
+                .stream()
+                .flatMap(JsonDiff::flatten)
+                .collect(LinkedHashMap::new, (m, e) -> m.put("/" + e.getKey(), e.getValue()), LinkedHashMap::putAll);
+    }
+
+    private static Stream<Map.Entry<String, Object>> flatten(Map.Entry<String, Object> entry) {
+
+        if (entry == null) {
+            return Stream.empty();
+        }
+
+        if (entry.getValue() instanceof Map<?, ?>) {
+            Map<?, ?> properties = (Map<?, ?>) entry.getValue();
+            return properties.entrySet()
+                    .stream()
+                    .flatMap(e -> flatten(new AbstractMap.SimpleEntry<>(entry.getKey() + "/" + e.getKey(), e.getValue())));
+        }
+
+        if (entry.getValue() instanceof List<?>) {
+            List<?> list = (List<?>) entry.getValue();
+            return IntStream.range(0, list.size())
+                    .mapToObj(i -> new AbstractMap.SimpleEntry<String, Object>(entry.getKey() + "/" + i, list.get(i)))
+                    .flatMap(JsonDiff::flatten);
+        }
+        return Stream.of(entry);
+    }
+}
